cd18e9ba1f9600c259bcc44bfdbb569339eaf765
Meryeme <dobli.meryeme@gmail.com>
2023-11-27 13:00:30 -0800

    task12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments: function () {
+    getNumberOfDepartments() {
       return Object.keys(this.allEmployees).length;
     },
   };
