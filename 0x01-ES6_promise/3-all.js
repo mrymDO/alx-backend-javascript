96b5cb3dc55dab3ba6048660b3e9f59e83d6aee6
Meryeme <dobli.meryeme@gmail.com>
2023-11-28 12:17:58 -0800

    task3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ const { uploadPhoto, createUser } = require('./utils');
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.photoId} ${userResponse.firstName} ${userResponse.lastName}`);
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => {
       console.error('Signup system offline');
