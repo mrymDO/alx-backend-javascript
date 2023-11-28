fb55487e06db4815abbf923cd3725e17233c3de5
Meryeme <dobli.meryeme@gmail.com>
2023-11-28 13:49:31 -0800

    fix task8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      reject(new Error('Cannot divide by 0'));
-    } else {
-      resolve(numerator / denominator);
-    }
-  });
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
 }
