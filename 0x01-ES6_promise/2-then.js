a879b7d320f73b881cecf6d969bade2fa0b36a51
Meryeme <dobli.meryeme@gmail.com>
2023-11-28 11:49:58 -0800

    task2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch((error) => {
+      console.error('Got an error from the API:', error.message);
+      return new Error();
+    });
+}
