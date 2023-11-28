e1f86b18a99eec872df4dee786ee17ce29215f42
Meryeme <dobli.meryeme@gmail.com>
2023-11-28 12:52:12 -0800

    fix task5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(fileName) {
   return new Promise((resolve, reject) => {
-    reject(new Error(`Error: ${fileName} cannot be processed`));
+    reject(new Error(`${fileName} cannot be processed`));
   });
 }
