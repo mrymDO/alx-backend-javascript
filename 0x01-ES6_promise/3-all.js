7ff7239bb8d330bbd31f0c1cf045dfa5c6963c75
Meryeme <dobli.meryeme@gmail.com>
2023-11-29 02:43:22 -0800

    test fix task3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((value) => {
-      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
+
     .catch(() => console.log('Signup system offline'));
 }
