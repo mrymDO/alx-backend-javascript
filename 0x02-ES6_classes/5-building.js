13c944eee2ba196ec7c84ade5cd734b0c42b5ef6
Meryeme <dobli.meryeme@gmail.com>
2023-11-30 11:46:30 -0800

    task5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number.');
+    }
+    return this._sqft;
+  }
+}
