85c6a9b47d89d60485c8fbf32e8142669864403d
Meryeme <dobli.meryeme@gmail.com>
2023-12-05 05:46:45 -0800

    task8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result += `${value.slice(startString.length)}-`;
+    }
+  });
+  if (result.length > 0) {
+    result = result.slice(0, -1);
+  }
+  return result;
+}
