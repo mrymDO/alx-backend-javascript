82d8e74140afd7e2052f55f94b5361a1212d5818
Meryeme <dobli.meryeme@gmail.com>
2024-01-30 00:58:56 -0800

    task1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
+  if (answer.trim() === '') {
+    rl.close();
+  } else {
+    console.log(`Your name is: ${answer}`);
+    rl.close();
+  }
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+});
