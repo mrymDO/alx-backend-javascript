a94d19b5397c01a404aee26310a2904454060406
Meryeme <dobli.meryeme@gmail.com>
2024-01-30 01:16:37 -0800

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,19 +1,10 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  console.log(`Your name is: ${input}`);
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  if (answer.trim() === '') {
-    rl.close();
-  } else {
-    console.log(`Your name is: ${answer}`);
-    rl.close();
-  }
-});
-
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
