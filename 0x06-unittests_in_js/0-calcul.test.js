d16a94d964e9cabd30ac00c3997ad49a3b16b1d4
Meryeme <dobli.meryeme@gmail.com>
2024-01-31 07:14:38 -0800

    fix task0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,9 @@ const calculateNumber = require('./0-calcul');
 
 describe('test calculate number function', () => {
   it('sum of two integers', () => {
-    assert.equal(calculateNumber(1, 1), 2);
+    assert.strictEqual(calculateNumber(1, 1), 2);
   });
   it('sum of two floats', () => {
-    assert.equal(calculateNumber(1.7, 1.7), 4);
+    assert.strictEqual(calculateNumber(1.7, 1.7), 4);
   });
 });
