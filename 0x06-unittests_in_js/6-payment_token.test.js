82ce62db8b5422879e746fc9e28c51da912ef5e0
Meryeme <dobli.meryeme@gmail.com>
2024-01-31 12:27:11 -0800

    fix task6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,10 +2,10 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('resolved to true', () => (done) => {
+  it('resolved to true', (done) => {
     getPaymentTokenFromAPI(true)
-      .then((result) => {
-        expect(result).to.deep.equal({ data: 'Successful response from the API' });
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
         done();
       });
   });
