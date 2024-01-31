25f2f43fc45a15663aead180c477552dfb333cda
Meryeme <dobli.meryeme@gmail.com>
2024-01-31 02:02:13 -0800

    fix task5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,64 +4,58 @@ const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
+function countStudents(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
 
-  if (request.url === '/') {
-    response.write('Hello Holberton School!');
-    response.end();
+      const lines = data.trim().split('\n').filter(Boolean);
+      const students = {};
+      const fields = {};
+
+      for (let i = 1; i < lines.length; i++) {
+        const [firstname, lastname, age, field] = lines[i].split(',');
+        if (!students[field]) students[field] = [];
+        students[field].push(firstname);
+        fields[field] = (fields[field] || 0) + 1;
+      }
+
+      const output = [`Number of students: ${lines.length - 1}`];
+      for (const [field, count] of Object.entries(fields)) {
+        output.push(`Number of students in ${field}: ${count}. List: ${students[field].join(', ')}`);
+      }
+
+      resolve(output.join('\n'));
+    });
+  });
+}
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
     return;
   }
 
-  if (request.url === '/students') {
-    response.write('This is the list of our students\n');
-
-    const filePath = process.argv[2].toString();
-
-    fs.promises.readFile(filePath, 'utf8')
-      .then((data) => {
-        const students = {};
-        const fields = {};
-        let length = 0;
-
-        const lines = data.split('\n');
-        for (let i = 0; i < lines.length; i += 1) {
-          if (lines[i]) {
-            length += 1;
-            const field = lines[i].split(',');
-
-            if (students[field[3]]) {
-              students[field[3]].push(field[0]);
-            } else {
-              students[field[3]] = [field[0]];
-            }
-
-            fields[field[3]] = (fields[field[3]] || 0) + 1;
-          }
-        }
-
-        const l = length - 1;
-        response.write(`Number of students: ${l}\n`);
-        for (const [key, value] of Object.entries(fields)) {
-          if (key !== 'field') {
-            response.write(`Number of students in ${key}: ${value}. `);
-            response.write(`List: ${students[key].join(', ')}\n`);
-          }
-        }
-
-        response.end();
+  if (req.url === '/students') {
+    countStudents(process.argv[2])
+      .then(data => {
+        res.end(`This is the list of our students\n${data}`);
       })
-      .catch((err) => {
-        response.statusCode = 404;
-        response.end(`Cannot load the database\n${err.message}`);
+      .catch(err => {
+        res.statusCode = 404;
+        res.end(`Cannot load the database\n${err.message}`);
       });
-
     return;
   }
 
-  response.statusCode = 404;
-  response.end();
+  res.statusCode = 404;
+  res.end();
 });
 
 server.listen(port, hostname, () => {
